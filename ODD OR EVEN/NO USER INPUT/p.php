3196c1bf9425c95314f85a813be9761156a6ca4c
user404e <e404user@gmail.com>
2023-05-04 19:40:38 +0000

    PHP

---
+++ ODD OR EVEN/NO USER INPUT/p.php
@@ -0,0 +1,9 @@
+<?php
+    $num1 = 21;
+    if($num1%2==0){
+        echo "NUMBER IS EVEN";
+    }
+    else {
+        echo "NUMBER IS ODD";
+    }
+?>
---
+++ ODD OR EVEN/USER INPUT/p1.php
@@ -0,0 +1,10 @@
+<?php
+    $num = $_GET['num'];
+
+    if ($num%2==0){
+        echo "NUMBER IS EVEN";
+    }
+    else{
+        echo "NUMBER IS ODD";
+    }
+?>
