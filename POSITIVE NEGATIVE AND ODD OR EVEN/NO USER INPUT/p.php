4b5f228a72a1d7b531ad4ce19181cc36cd4d4510
user404e <e404user@gmail.com>
2023-05-05 14:30:42 +0000

    PHP

---
+++ POSITIVE NEGATIVE AND ODD OR EVEN/NO USER INPUT/p.php
@@ -0,0 +1,22 @@
+<?php
+
+$x = 23;
+
+if($x > 0){
+        echo "POSITIVE" . "<br>";
+}
+elseif($x == 0){
+    echo "ZERO" . "<br>";
+}
+  else{
+    echo "NEGATIVE" . "<br>";
+}
+
+if($x%2 == 0){
+        echo "EVEN" . "<br>";
+}
+  else{
+    echo "ODD" . "<br>";
+}
+
+?>
---
+++ POSITIVE NEGATIVE AND ODD OR EVEN/USER INPUT/p.php
@@ -0,0 +1,22 @@
+<?php
+
+$x = $_GET['x'];
+
+if($x > 0){
+        echo "POSITIVE" . "<br>";
+}
+elseif($x == 0){
+    echo "ZERO" . "<br>";
+}
+  else{
+    echo "NEGATIVE" . "<br>";
+}
+
+if($x%2 == 0){
+        echo "EVEN" . "<br>";
+}
+  else{
+    echo "ODD" . "<br>";
+}
+
+?>
