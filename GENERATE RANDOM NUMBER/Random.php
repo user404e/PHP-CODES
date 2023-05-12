098cdbd2fdb287e8e630cb6b61f89e75a8b3a443
user404e <e404user@gmail.com>
2023-05-12 10:37:17 +0000

    PHP

---
+++ GENERATE RANDOM NUMBER/Random.php
@@ -0,0 +1,6 @@
+<?php
+    $start = $_POST['start'];
+    $end = $_POST['end'];
+
+    echo rand($start,$end);
+?>
---
+++ GENERATE RANDOM NUMBER/input.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Random.php" method="POST">
+        Enter Range - <br><br>
+
+        Starting : <input type="text" name="start"> <br><br>
+
+        Ending : <input type="text" name="end"> <br><br>
+
+        <input type='submit'>
+        
+    </form>
+</body>
+</html>
