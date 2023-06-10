0db21e672b1561625a6090c66bd4bb4f117d7b8a
user404e <e404user@gmail.com>
2023-06-10 11:44:03 +0000

    PHP

---
+++ NTH LARGEST NUMBER IN ARRAY/USER INPUT/input.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <form action="show.php" method="post">
+            Enter N : <input type="number" name="n"> <br><br>
+
+            <input type="submit">
+        </form>
+    </body>
+</html>
---
+++ NTH SMALLEST NUMBER IN ARRAY/USER INPUT/form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="show.php" method="POST">
+        
+        Enter N  : <input type="number" name="n"> <br><br>
+
+        <input type="submit">
+    </form>
+</body>
+</html>
